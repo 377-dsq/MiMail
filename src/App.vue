4e8e4ed1d649cc264853b8cf4e1558fc030c6cd3
Du Siqi <270836973@qq.com>
2020-01-01 16:36:40 +0800

    跨域

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+//import axios from 'axios'
+import jsonp from 'jsonp'
 export default {
   name: 'app',
   components: {
@@ -15,8 +16,16 @@ export default {
   },
   data(){
     return {
-      age:30
+      age:30,
+      data:''
     }
+  },
+  mounted(){
+    let url='/api/common/adver-getadver';
+    jsonp(url,(error,res)=>{
+      let result=res;
+      this.data=result;
+    })
   }
 }
 </script>
