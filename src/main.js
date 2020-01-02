a465dc6d74aa90c1a57a19524f8cc66f90d0bfea
Du Siqi <270836973@qq.com>
2020-01-02 22:33:13 +0800

    environment setting

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
-import router from './router'
+import router from './router' //./表示当前目录，没有./则表明是插件
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
+import env from './env'
 
-Vue.config.productionTip=false
+//根据前端的跨域方式做调整
+axios.defaults.baseURL='/api';
+axios.defaults.timeout=8000;  //超时设置
+//根据环境变量获取不同的请求地址
+axios.defaults.baseURL=env.baseURL;
+
+//接口错误拦截
+axios.interceptors.response.use(function(response){
+  let res=response.data; //接口返回的值,是个对象
+  if(res.status==0){
+    return res.data;
+  }else if(res.status==10){
+    window.location.href='/#/login';
+  }else{
+    alert(res.msg);
+  }
+});
+
+Vue.use(VueAxios,axios);
+Vue.config.productionTip=false  //生产环境的提示，
 
 new Vue({
   router,
--- src/storage/index.js
+++ src/storage/index.js
@@ -4,9 +4,16 @@ export default{
     //存储值
     setItem(key,value,module_name){
         if(module_name){
-            let val=this.getItem(module_name);
-            val[key]=value;
-            this.setItem(module_name,val)
+            if(!this.getItem(module_name)){
+                let val=this.getStorage();
+                val[module_name]=new Object();
+                val[module_name][key]=value;
+                window.sessionStorage.setItem(STORAGE_KEY,JSON.stringify(val));
+            }else{
+                let val=this.getItem(module_name);
+                val[key]=value;
+                this.setItem(module_name,val)
+            }
         }else{
             let val=this.getStorage();
             val[key]=value;
@@ -31,6 +38,7 @@ export default{
     clear(key,module_name){
         let val=this.getStorage();
         if(module_name){
+            if(!val[module_name]) return;
             delete val[module_name][key];
         }else{
             delete val[key];
